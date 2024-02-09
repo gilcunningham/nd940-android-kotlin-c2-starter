e1c82f86618a0435cbd6f953580eea11c7747669
Gil Cunningham <gilcunningham@gmail.com>
2024-02-08 22:55:46 -0500

    recycler_view : - asteroid adapter, list item, on click

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -0,0 +1,55 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.data.Asteroid
+import com.udacity.asteroidradar.databinding.AsteroidListItemBinding
+
+class AsteroidAdapter(private val listener: OnClickListener) :
+    ListAdapter<Asteroid, AsteroidAdapter.ViewHolder>(AsteroidDiffCallback()) {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), listener)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ) = ViewHolder(DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.asteroid_list_item,
+            parent,
+            false
+        )
+    )
+
+    class AsteroidDiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class ViewHolder(
+        private val binding: AsteroidListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(asteroid: Asteroid, listener : OnClickListener) {
+            binding.model = asteroid
+            binding.clickListener = OnClickListener { listener.onClick(asteroid) }
+        }
+    }
+
+    interface OnClickListener {
+        fun onClick(asteroid: Asteroid)
+    }
+}
+
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,23 +1,30 @@
 package com.udacity.asteroidradar.main
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(), MenuProvider {
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    private val viewModel : MainViewModel by viewModels()
+
+    private lateinit var asteroidAdapter : AsteroidAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requireActivity().addMenuProvider(this, this, Lifecycle.State.RESUMED)
+        asteroidAdapter = AsteroidAdapter(viewModel.asteroidListClickListener)
     }
 
     override fun onCreateView(
@@ -27,6 +34,8 @@ class MainFragment : Fragment(), MenuProvider {
     ) = FragmentMainBinding.inflate(inflater).apply {
         lifecycleOwner = this@MainFragment
         viewModel = this@MainFragment.viewModel
+        asteroidRecycler.layoutManager = LinearLayoutManager(this@MainFragment.context)
+        asteroidRecycler.adapter = asteroidAdapter
     }.root
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
@@ -34,15 +43,15 @@ class MainFragment : Fragment(), MenuProvider {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.asteroid.observe(viewLifecycleOwner) { asteroids ->
-            asteroids.forEach {
-                println("** ${it.codename}")
+        viewModel.asteroids.observe(viewLifecycleOwner) { asteroids ->
+            asteroids?.let {
+                asteroidAdapter.submitList(asteroids)
             }
         }
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        println("*** on menu")
+
         return true
     }
 }
