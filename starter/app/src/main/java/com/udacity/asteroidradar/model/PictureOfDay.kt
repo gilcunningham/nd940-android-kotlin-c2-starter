be76666a90edab1fbca4bacec57d717d36595c10
Gil Cunningham <gilcunningham@gmail.com>
2024-02-12 21:07:24 -0500

    review cleanup (#13)

--- starter/app/src/main/java/com/udacity/asteroidradar/model/PictureOfDay.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/model/PictureOfDay.kt
@@ -1,7 +1,13 @@
 package com.udacity.asteroidradar.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.udacity.asteroidradar.R
+
 data class PictureOfDay(
     val mediaType: String,
     val title: String,
-    val url: String
+    val url: String,
+    @StringRes
+    val doneLoadingDescriptionRes : Int = R.string.nasa_picture_of_day_content_description_format
 )
